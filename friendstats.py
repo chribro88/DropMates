519d314ac4599f55b3930354bae003005efce912
Mircea Mihalea <mircea@mihalea.ro>
2017-05-18 22:15:26 +0100

    Add Application stub
    Constructor, read_cache and close methods are created

--- friendstats.py
+++ friendstats.py
@@ -5,6 +5,44 @@
 # atexit.register(logout)
 import argparse
 
+from FileRepository import read_json
+from SessionController import SessionController
+
+
+class Application:
+    filestore = "cache.json"
+
+    def __init__(self, username, password, rebuild):
+        self.session = SessionController()
+        self.followers = []
+        self.following = []
+
+        # self.session.login(username, password)
+        if not rebuild:
+            self.read_cache()
+
+        if not self.followers and not self.following:
+            self.rebuild_cache()
+
+    def read_cache(self):
+        cache = read_json(Application.filestore)
+
+        if cache:
+            try:
+                self.followers = cache['followers']
+                self.following = cache['following']
+                return True
+            except:
+                print("Failed to read cache")
+
+        return False
+
+    def rebuild_cache(self):
+        pass
+
+    def close(self):
+        self.session.logout()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Analyze and process instagram followers')
 
@@ -20,3 +58,19 @@ if __name__ == "__main__":
     group.add_argument("-s", "--shame", action="store_true", help="Show those users that don't follow you back")
 
     args = parser.parse_args()
+
+    app = None
+    username = args.username
+    password = args.password
+    if not username and not password and args.config:
+        config = read_json(args.config)
+        username = config['username']
+        password = config['password']
+
+    if not username and not password:
+        print("No credentials found")
+        exit()
+
+    app = Application(username, password, args.rebuild)
+
+    app.close()
