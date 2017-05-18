e08a0a73e6b579e912e223b1d352280cd219ab1b
Mircea Mihalea <mircea@mihalea.ro>
2017-05-18 19:14:21 +0100

    Add argparse

--- friendstats.py
+++ friendstats.py
@@ -1,82 +1,22 @@
 #!/usr/bin/env python3
 
-import requests
-import random
-import time
-import json
-import atexit
-import signal
-import sys
-import datetime
-import os.path
 
-from SessionController import SessionController
+# signal.signal(signal.SIGTERM, logout)
+# atexit.register(logout)
+import argparse
 
-if not os.path.isfile('config.json'):
-    print('please create a config.json file (config_sammple.json in root)')
-    quit()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Analyze and process instagram followers')
 
-with open('config.json', 'r') as fp:
-    try:
-        config = json.load(fp)
-    except:
-        print('invalid config.json file (probably parsing issue)')
-        quit()
+    parser.add_argument("-u", "--username", help='Instagram username')
+    parser.add_argument("-p", "--password", help='Instagram password')
+    parser.add_argument("-c", "--config", help='Application config file')
+    parser.add_argument("-r", "--rebuild", action="store_true", help='Rebuild the user cache from the web')
+    parser.add_argument("-v", "--verified", action="store_true", help='Filter by hiding verified users')
 
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-f", "--followers", action="store_true", help="Show your followers")
+    group.add_argument("-o", "--following", action="store_true", help="Show the user that you follow")
+    group.add_argument("-s", "--shame", action="store_true", help="Show those users that don't follow you back")
 
-session_ctrl = SessionController()
-
-
-def start_sync():
-    login()
-
-    user_followers = session_ctrl.get_followers()
-
-    user_following = session_ctrl.get_following()
-
-    diff = set(user_following) - set(user_followers)
-
-    print('found %i people who you follow but don\'t follow you back' % (len(diff)))
-
-    with open('state.json', 'w') as user_state:
-        print('writing data to state.json')
-
-        json.dump({
-            'version': '0.0.1',
-            'state_date': datetime.datetime.now().isoformat(),
-            'followers': {
-                'data': user_followers
-            },
-            'following': {
-                'data': user_following
-            }
-        }, user_state, indent=4, sort_keys=True)
-        user_state.close()
-
-
-def load_sync():
-    if not os.path.isfile('state.json'):
-        print('no state.json to load, please run a sync with option 1 or 2')
-        quit()
-
-    fp = open('state.json', 'r')
-    user_state = json.load(fp)
-    fp.close()
-
-    unfollow_list = set(user_state['following']['data']) - set(user_state['followers']['data'])
-
-    if len(unfollow_list) <= 0:
-        print('nobody to unfollow')
-        quit()
-
-    session_ctrl.unfollow_all(unfollow_list)
-
-
-
-
-
-
-signal.signal(signal.SIGTERM, logout)
-atexit.register(logout)
-
-menu()
+    args = parser.parse_args()
