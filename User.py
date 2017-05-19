386ef12c8464dab7da09395e1e9267e1edd8b0e1
Mircea Mihalea <mircea@mihalea.ro>
2017-05-19 13:56:28 +0100

    Fix bug in User constructor
    Construction function was misspelled

--- User.py
+++ User.py
@@ -1,7 +1,12 @@
+import logging
+
+logger = logging.getLogger('stats')
+
+
 class User:
-    def __int__(self, id, username, full_name, is_verified, followed_by_viewer,
-                requested_by_viewer, profile_pic_url):
-        self.id = id
+    def __init__(self, user_id, username, full_name, is_verified, followed_by_viewer,
+                 requested_by_viewer, profile_pic_url):
+        self.id = user_id
         self.username = username
         self.full_name = full_name
         self.is_verified = is_verified
@@ -11,7 +16,8 @@ class User:
 
     @classmethod
     def from_node(cls, node):
+        logger.debug("Creating User from node")
+        logger.debug(node)
         return cls(node['id'], node['username'], node['full_name'], node['is_verified'],
-                   node['followed_by_viewer'], node['requested_by_viewer'],
-                   node['profile_pic_url'])
+                   node['followed_by_viewer'], node['requested_by_viewer'], node['profile_pic_url'])
 
